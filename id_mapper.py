6224016289a893f647fa255fffedb280f972f356
shasini274 <52023868+shasini274@users.noreply.github.com>
2019-08-02 08:17:50 +0000

    Update id_mapper.py

--- id_mapper.py
+++ id_mapper.py
@@ -1,28 +1,16 @@
-import hashlib
-import json
-import logging
 import traceback
-import datetime
 
-import mysql.connector
-from mysql.connector import Error
-from utils import \
-  map_curw_id, \
-  check_runname
-
-
+from mysqladapter1 import check_runname
+from utils import map_curw_id
 
 #get the curw_ids from the curw database where run_name only equals to A&T Labs/CUrW IoT/Leecom
 result = check_runname('%A&T Labs', '%CUrW IoT', '%Leecom')
 print(result)
 for id in result:
- try :
-   map_curw_id(id[0])
- except Exception as e:
-   traceback.print_exc()
-
-
-
+    try:
+        map_curw_id(id[0])
+    except Exception as e:
+        traceback.print_exc()
 
 
 
